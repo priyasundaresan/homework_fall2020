093eea4731e8d1921832f12974609602b3232e5c
Priya Sundaresan <priyasundaresan@berkeley.edu>
2020-11-21 18:10:49 -0800

    pushing q1

---
+++ hw5/q1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 cs285/scripts/run_hw5_expl.py --env_name PointmassEasy-v0 --use_rnd --unsupervised_exploration --exp_name q1_env1_rnd
+python3 cs285/scripts/run_hw5_expl.py --env_name PointmassEasy-v0 --unsupervised_exploration --exp_name q1_env1_random
+python3 cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --use_rnd --unsupervised_exploration --exp_name q1_env2_rnd
+python3 cs285/scripts/run_hw5_expl.py --env_name PointmassMedium-v0 --unsupervised_exploration --exp_name q1_env2_random
